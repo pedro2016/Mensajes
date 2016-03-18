8c5153e040782da7c398083124e13d88a65232b3
Bersek87 <lrtch87@gmail.com>
2016-03-18 15:21:45 -0600

    datos de perfil ingresados

--- app/src/main/java/com/example/lizbeth/mensajes/Personas.java
+++ app/src/main/java/com/example/lizbeth/mensajes/Personas.java
@@ -3,6 +3,7 @@ package com.example.lizbeth.mensajes;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * Created by Victor on 29/02/2016.
  */
@@ -117,16 +118,16 @@ public class Personas {
                 R.string.facebookP1,
                 R.string.twitterP1)
         );
-        PERSONAS_LIST.add(new Personas(R.string.nombreP2,
-                R.string.descripcionP2,
-                R.string.edadP2,
-                R.string.profesionP2,
-                R.drawable.aleman,
-                R.string.estadocivilP2,
-                R.string.numeroP2,
-                R.string.emailP2,
-                R.string.facebookP2,
-                R.string.twitterP2)
+        PERSONAS_LIST.add(new Personas(R.string.nombreP6,
+                R.string.descripcionP6,
+                R.string.edadP6,
+                R.string.profesionP6,
+                R.drawable.luis,
+                R.string.estadocivilP6,
+                R.string.numeroP6,
+                R.string.emailP6,
+                R.string.facebookP6,
+                R.string.twitterP6)
         );
         PERSONAS_LIST.add(new Personas(R.string.nombreP3,
                 R.string.descripcionP3,
