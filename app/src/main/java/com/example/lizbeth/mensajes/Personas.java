3bc01e062d5ec4e2504a7a02fd1eef9898b512be
joelmsa <joel.mejia@ujcv.edu.hn>
2016-03-18 15:19:09 -0600

    ingreso de la foto perfil

--- app/src/main/java/com/example/lizbeth/mensajes/Personas.java
+++ app/src/main/java/com/example/lizbeth/mensajes/Personas.java
@@ -77,7 +77,7 @@ public class Personas {
                 R.string.descripcionP3,
                 R.string.edadP3,
                 R.string.profesionP3,
-                R.drawable.cesarlo,
+                R.drawable.cesar2_0,
                 R.string.estadocivilP3,
                 R.string.numeroP3,
                 R.string.emailP3,
