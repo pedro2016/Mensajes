de6de6201f4eb4886fff4956a23613d2f2a431d0
Aleman1414 <angel.aleman1414@gmail.com>
2016-03-18 15:46:41 -0600

    cambiado foto de perfil

--- app/src/main/java/com/example/lizbeth/mensajes/Personas.java
+++ app/src/main/java/com/example/lizbeth/mensajes/Personas.java
@@ -67,7 +67,7 @@ public class Personas {
                 R.string.descripcionP2,
                 R.string.edadP2,
                 R.string.profesionP2,
-                R.drawable.aleman1,
+                R.drawable.aleman,
                 R.string.estadocivilP2,
                 R.string.numeroP2,
                 R.string.emailP2,
